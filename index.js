dfc9c172fbcc2acef52cf76c51d5972ef8950dab
stanley <stanley.njuguna@student.moringaschool.com>
2024-01-14 09:43:34 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+
+
+function moveDodgerLeft() {
+    const left = dodger.style.left.replace("px", "");
+    const leftValue = parseInt(left, 10);
+
+    if (leftValue > 0) {
+      dodger.style.left = `${leftValue - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const left = dodger.style.left.replace("px", "");
+    const leftValue = parseInt(left, 10);
+
+    if (leftValue < 360) {
+      dodger.style.left = `${leftValue + 1}px`;
+    }
+  }
